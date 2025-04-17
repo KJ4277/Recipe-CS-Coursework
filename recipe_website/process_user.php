0ccec381df7b08ec317dd57ca39d7eb110a85fee
KJ4277 <kjohri4@gmail.com>
2025-04-17 19:54:50 +0100

    Update process_user.php

--- recipe_website/process_user.php
+++ recipe_website/process_user.php
@@ -15,10 +15,11 @@ mysqli_stmt_execute($stmt);
 $result_username = mysqli_stmt_get_result($stmt);
 
 if (mysqli_num_rows($result_username) > 0) {
+    // Username already exists, redirect to the registration page with an error
     header("Location: add_user.php?error=userexists");
     exit();
 } else {
-    // Hash the password
+    // Hash the password before saving to the database
     $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
     // Insert user into the database using a prepared statement
@@ -27,12 +28,18 @@ if (mysqli_num_rows($result_username) > 0) {
     mysqli_stmt_bind_param($stmt, "ss", $username, $hashed_password);
 
     if (mysqli_stmt_execute($stmt)) {
-        // Set session and redirect to main page
+        // Set session and redirect to the main page
         $_SESSION['username'] = $username;
+        $_SESSION['user_id'] = mysqli_insert_id($mysqli);  // Get the ID of the inserted user
         header("Location: main_page.php");
         exit();
     } else {
+        // Error occurred during insertion
         echo "Error: " . mysqli_error($mysqli);
     }
 }
+
+// Close the statement and connection
+mysqli_stmt_close($stmt);
+mysqli_close($mysqli);
 ?>
